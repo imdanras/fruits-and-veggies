66141ef0c4c25aab20bc84013f6d5dabeeca6ab8
Daniel Rasmussen <dan.r.rasmussen@gmail.com>
2016-07-21 21:44:40 -0700

    works except the colors

--- js/app.js
+++ js/app.js
@@ -4,3 +4,44 @@
 console.log('App Started');
 console.log('Fruit count', fruits.length);
 console.log('Veggie count', vegetables.length);
+
+var myApp = angular.module('myApp', []);
+
+myApp.controller('myCtrl', ['$scope', function($scope) {
+  $scope.fruits = [];
+  $scope.veggies = [];
+  $scope.foodArray = fruits.concat(vegetables);
+
+  $scope.moveLeft = function(idx) {
+    $scope.fruits.push($scope.foodArray[idx]);
+    $scope.foodArray.splice(idx, 1);
+  }
+
+  $scope.moveRight = function(idx) {
+    $scope.veggies.push($scope.foodArray[idx]);
+    $scope.foodArray.splice(idx, 1);
+  }
+
+  $scope.moveBackLeft = function(idx) {
+    $scope.foodArray.push($scope.veggies[idx]);
+    $scope.veggies.splice(idx, 1);
+  }
+
+  $scope.moveBackRight = function(idx) {
+    $scope.foodArray.push($scope.fruits[idx]);
+    $scope.fruits.splice(idx, 1);
+  }
+
+}]);
+
+// myApp.directive('arrowLeft', function() {
+//   return {
+//     templateUrl: 'templates/arrowLeft.html'
+//   }
+// })
+
+// myApp.directive('arrowRight', function() {
+//   return {
+//     templateUrl: 'templates/arrowRight.html'
+//   }
+// })
